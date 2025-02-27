7d8d8a9d1312f03734bf10bfe1d999dc20dc060c
Istvan Toth <tott.isti@gmail.com>
2025-02-27 15:29:31 +0100

    disable GPU

--- tutorial-mnist.py
+++ tutorial-mnist.py
@@ -1,12 +1,32 @@
 import tensorflow as tf
 mnist = tf.keras.datasets.mnist
 
+
+
+
+physical_devices = tf.config.list_physical_devices('GPU')
+try:
+  # Disable first GPU
+  tf.config.set_visible_devices(physical_devices[1:], 'GPU')
+  logical_devices = tf.config.list_logical_devices('GPU')
+  # Logical device was not created for first GPU
+  assert len(logical_devices) == len(physical_devices) - 1
+except:
+  # Invalid device or cannot modify virtual devices once initialized.
+  pass
+
+
+
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(2048, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(2048, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(2048, activation='relu'),
   tf.keras.layers.Dropout(0.2),
   tf.keras.layers.Dense(10, activation='softmax')
 ])
