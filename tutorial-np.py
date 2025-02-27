8f371b03e3d87ffe5ae0521d4eb1e726ca3013f0
itoth1 <istvan.toth2@eos-hungary.hu>
2025-02-27 16:31:13 +0100

    smaller data

--- tutorial-np.py
+++ tutorial-np.py
@@ -1,10 +1,16 @@
 import numpy as np
 import time
-
-np_random_array1 = np.random.rand(6400, 6400)
-np_random_array2 = np.random.rand(6400, 6400)
+import sys
 
 s = time.perf_counter()
-np_mul = np.dot(np_random_array1, np_random_array2)
+np_random_array1 = np.random.rand(8192, 8192 * 4)
+print(sys.getsizeof(np_random_array1) / 1024 / 1024 / 1024, 'GB')
 e = time.perf_counter()
-print(e-s)
+print(e-s, ' sec')
+np_random_array2 = np.random.rand(8192, 8192 * 4)
+print(sys.getsizeof(np_random_array2) / 1024 / 1024 / 1024, 'GB')
+e = time.perf_counter()
+print(e-s, ' sec')
+#np_mul = np.dot(np_random_array1, np_random_array2)
+e = time.perf_counter()
+print(e-s, ' sec')
